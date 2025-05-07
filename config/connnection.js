16635de16f21e539c92546ce8167a18a650a3c12
dhirendrabam001 <dhirendrabam12345@gmail.com>
2025-05-07 00:04:49 -0700

    config with connection.js

--- config/connnection.js
+++ config/connnection.js
@@ -8,14 +8,23 @@
 
 
 
+
+
+// const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost:27017/userdata";
+
+// mongoose.connect(MONGODB_URI, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true
+// })
+// .then(() => console.log('Connected to MongoDB'))
+// .catch((err) => console.error('MongoDB connection error:', err));
+
 const mongoose = require('mongoose');
 
-const MONGODB_URI = process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/userdata";
-
-mongoose.connect(MONGODB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-})
-.then(() => console.log('Connected to MongoDB'))
-.catch((err) => console.error('MongoDB connection error:', err));
-
+const dB = "mongodb+srv://dhirendrabam12345:%40Dhirendra123@portfolio.f9wmcck.mongodb.net/?retryWrites=true&w=majority&portfolio";
+mongoose.connect(dB).then(() => {
+    console.log('Connection Sucussfully');
+}).catch((error) => {
+    console.log(`No Connection DB`)
+    
+});
