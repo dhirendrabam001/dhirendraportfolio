4747f529b226a936301f5b6bea90fd304f8a51ba
dhirendrabam001 <dhirendrabam12345@gmail.com>
2025-05-07 01:28:27 -0700

    update connection.js

--- config/connnection.js
+++ config/connnection.js
@@ -9,16 +9,6 @@
 
 
 
-
-// const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost:27017/userdata";
-
-// mongoose.connect(MONGODB_URI, {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true
-// })
-// .then(() => console.log('Connected to MongoDB'))
-// .catch((err) => console.error('MongoDB connection error:', err));
-
 const mongoose = require('mongoose');
 
 const dB = "mongodb+srv://dhirendrabam12345:%40Dhirendra123@portfolio.f9wmcck.mongodb.net/?retryWrites=true&w=majority&appName=portfolio";
@@ -28,3 +18,5 @@ mongoose.connect(dB).then(() => {
     console.log(`No Connection DB`)
     
 });
+
+
